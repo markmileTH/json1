367a6c0df599a6be8b4abc605599cc5c8bae25f7
markmileTH <73652040+markmileTH@users.noreply.github.com>
2021-12-07 14:57:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,60 +4,111 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <script src="https://code.jquery.com/jquery-3.6.0.js"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 
 <body>
 
+    <button id="btnBack"> back </button>
+
+    <div id="main">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                </tr>
+            </thead>
+            <tbody id="tblPost">
+            </tbody>
+        </table>
+    </div>
+
+
+    <div id="detail">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>UserID</th>
+                </tr>
+            </thead>
+            <tbody id="tblDetails">
+            </tbody>
+        </table>
+    </div>
+
+
 </body>
 
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>fname</th>
-            <th>lname</th>
-        </tr>
-    </thead>
-    <tbody id="tblstudent">
 
-    </tbody>
-</table>
 <script>
 
-    function loadJSON() {
+    function refresh() {
         
-        var url ="http://cj-android-demon.herokuapp.com/json2.php"
-        
-        $.getJSON(url).done((data)=>{
-            console.log(data);
-            $.each(data, (k,item) =>{
-                console.log(k);
-                console.log(item);
-                var line = "<tr>";
-                    line += "<td>" + (k+1) + "</td>";
-                    line += "<td>" + item.fname + "</td>";
-                    line += "<td>" + item.lname + "</td>";
-                    line += "</tr>";
-                $("#tblstudent").append(line);
-            })
-        })
-        .fail((xhr, status, err)=>{
-            console.log("error");
-        });
     }
 
+    function showDetails(id) {
+        $("#main").hide();
+        $("#detail").show();
+
+        // console.log(id);
+        var url = "https://jsonplaceholder.typicode.com/posts/" + id
+
+        $.getJSON(url)
+            .done((data) => {
+                console.log(data);
+                var line = "<tr id='detailROW'";
+                    line += "><td>" + data.id + "</td>"
+                    line += "<td><b>" + data.title + "</b><br/>"
+                    line += data.body + "</td>"
+                    line += "<td>" + data.userId + "</td>"
+                    line += "</tr>";
+                    $("#tblDetails").append(line);
+            })
+            .fail((xhr, err, status) => {
+
+            })
 
 
+    }
 
+    function LoadPosts() {
+        var url = "https://jsonplaceholder.typicode.com/posts"
+
+        $.getJSON(url)
+            .done((data) => {
+                $.each(data, (k, item) => {
+                    // console.log(item);
+                    var line = "<tr>";
+                    line += "<td>" + item.id + "</td>"
+                    line += "<td><b>" + item.title + "</b><br/>"
+                    line += item.body + "</td>"
+                    line += "<td><button onClick='showDetails(" + item.id + ");'>Link</button></td>"
+                    line += "</tr>";
+                    $("#tblPost").append(line);
+                });
+                $("#main").show();
+            })
+            .fail((xhr, err, status) => {
+
+            })
+    }
 
     $(() => {
-        loadJSON();
-    });
-
+        LoadPosts();
+        $("#detail").hide();
+        $("#btnBack").click(() => {
+            $("#main").show();
+            $("#detail").hide();
+            $("#detailROW").remove();
+        });
+    })
 </script>
 
 </html>
